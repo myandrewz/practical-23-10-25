c5c45de89e29bf209fd15623e293a551059770c2
dison <d.lyadda@oginnovation.com>
2025-10-23 09:36:27 +0300

    Add project files.

---
+++ NssfTechInterview/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using NssfTechInterview.BusinessLogic;
+
+
+BusinessLogic logic = new BusinessLogic();
+
+var list = logic.GetCombinations(4, 2);
+
+// Print the results
+foreach (var combination in list)
+{
+    Console.WriteLine("[" + string.Join(",", combination) + "]");
+}
