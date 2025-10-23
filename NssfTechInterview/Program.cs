f32e078bc12f28d2629f51def812362ad8381d3a
dison <d.lyadda@oginnovation.com>
2025-10-23 10:46:05 +0300

    Removed unused class

--- NssfTechInterview/Program.cs
+++ NssfTechInterview/Program.cs
@@ -1,14 +1,3 @@
 ﻿
-using NssfTechInterview.BusinessLogic;
 
-
-int n = 9;
-ZeroEvenOdd zeroEvenOdd = new ZeroEvenOdd(n);
-
-// Run all threads in parallel
-Task t1 = Task.Run(() => zeroEvenOdd.Zero(Console.Write));
-Task t2 = Task.Run(() => zeroEvenOdd.Even(Console.Write));
-Task t3 = Task.Run(() => zeroEvenOdd.Odd(Console.Write));
-
-Task.WaitAll(t1, t2, t3);
 Console.WriteLine(); // Move to next line after output
