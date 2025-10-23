065bde6683bc153ae5dd1ad1b9d331b3dd6b44d4
Bright Kanyange <brightkanyange@outlook.com>
2025-10-23 09:35:13 +0300

    initial: commit

---
+++ src/test/java/dev/brightkan/ticket_line/TicketLineApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.brightkan.ticket_line;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
